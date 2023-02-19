d3a3979ece20f56d9da3345cf1c44f84fa2ab91b
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-02-19 17:19:12 +0200

    rain python

---
+++ rain/0-rain.py
@@ -0,0 +1,20 @@
+def rain(walls):
+    if not walls:
+        return 0
+
+    left, right = 0, len(walls) - 1
+    left_max, right_max = walls[left], walls[right]
+    total = 0
+
+    while left < right:
+        if walls[left] < walls[right]:
+            left_max = max(left_max, walls[left])
+            total += left_max - walls[left]
+            left += 1
+        else:
+            right_max = max(right_max, walls[right])
+            total += right_max - walls[right]
+            right -= 1
+
+    return total
+
