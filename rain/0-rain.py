5a3fed3a8695e0291b1821ceff18fd514c35d665
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-02-20 17:35:27 +0200

    rain

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,37 +1,31 @@
 
 #!/usr/bin/python3
-"""
-    Find the maximum amount of rain that the array's walls can withstand.
-"""
+
 
 
 def rain(walls):
-    """
-        Find the maximum amount of rain that the array's walls can withstand.
-    """
-    water_temp = 0
+    w = 0
     water = 0
     size = len(walls)
     prev_wall = 0
 
-    # If list is empty return 0.
     if size <= 0:
-        return water_temp
+        return w
 
     for a in range(size):
 
         if walls[a] >= walls[prev_wall]:
             prev_wall = a
-            water_temp = 0
+            w = 0
         else:
             water += walls[prev_wall] - walls[a]
-            water_temp += walls[prev_wall] - walls[a]
+            w += walls[prev_wall] - walls[a]
 
     if prev_wall < size - 1:
 
-        # Subtract last temp volume from total.
-        water -= water_temp
-        # Store last peak wall.
+    
+        water -= w
+    
         prev_pass_peak = prev_wall
         prev_wall = size - 1
 
